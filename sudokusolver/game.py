42d5eaf433f1a8d99434785cd1ea977aa1dd69a4
Perry Hertler <perry@hertler.org>
2020-03-24 21:05:05 -0600

    refactoring for readability

--- sudokusolver/game.py
+++ sudokusolver/game.py
@@ -1,5 +1,10 @@
 from sudokusolver.board import Board
 
+
+def possible_values(matrix, row, col):
+    return Board(matrix).potential_values_for_position(row, col)
+
+
 class Game:
     def __init__(self, puzzle):
         self.puzzle = puzzle
@@ -10,16 +15,14 @@ class Game:
     def solve_it(self, matrix):
         for row in range(9):
             for col in range(9):
-                if matrix[row][col] != 0:
-                    continue
-                for candidate in self.possible_values(matrix, row, col):
+                if matrix[row][col] != 0: continue
+
+                for candidate in possible_values(matrix, row, col):
                     matrix[row][col] = candidate
-                    if self.solve_it(matrix):
-                        return matrix
+                    if self.solve_it(matrix): return matrix
+
                     matrix[row][col] = 0 # back to original value
                 return   # nothing worked
-        return matrix
+        return matrix 
 
 
-    def possible_values(self, matrix, row, col):
-        return Board(matrix).potential_values_for_position(row, col)
